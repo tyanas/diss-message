808073351efde007a329c21b0ff4f31e31669e0a
tyanas <tyanas.go@gmail.com>
2013-07-22 12:26:44 +0400

    keep page number

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
 (function($){
+  var page = window.location.hash;
+
+  function updateUrl(event) {
+    window.location.hash = event.index;
+    return false;
+  }
+
   bespoke.vertical.from('article');
+  if (page) {
+    page = page.substr(page.length-1);
+    bespoke.slide(parseInt(page));
+  }
+  bespoke.on('activate', updateUrl);
 })(jQuery);
